ba764c08872873d3113c3877b278ba8a120db1a0
Walter Reiner <walter.reiner@wreiner.at>
2021-07-18 08:42:31 +0200

    Add versions of helm charts

--- deploy-mariadb.sh
+++ deploy-mariadb.sh
@@ -1,4 +1,4 @@
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo update
 
-helm install --namespace nextcloud -f mariadb-values.yml mariadb bitnami/mariadb
+helm install --namespace nextcloud -f mariadb-values.yml --version 9.3.17 mariadb bitnami/mariadb
--- deploy-nextcloud.sh
+++ deploy-nextcloud.sh
@@ -1,4 +1,4 @@
 helm repo add nextcloud https://nextcloud.github.io/helm/
 helm repo update
 
-helm install --namespace nextcloud -f nextcloud-values.yml nextcloud nextcloud/nextcloud
+helm install --namespace nextcloud -f nextcloud-values.yml --version 2.6.5 nextcloud nextcloud/nextcloud
